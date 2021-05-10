f78c191b4c1b8b0aa5948d2ed9d40cb4a3540ab3
Cédric BOISSINOT <cedric.boissinot99@gmail.com>
2021-05-10 17:51:46 +0200

    change table listArtist

--- src/components/list-artists/list-artists.component.tsx
+++ src/components/list-artists/list-artists.component.tsx
@@ -1,4 +1,5 @@
 import React, { Fragment, FunctionComponent, useEffect, useState } from 'react';
+import { Table } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
 
 
@@ -25,14 +26,25 @@ const ListArtistComponent: FunctionComponent<IListArtistComponentProps> = (props
         <table>
             {
                 artists.map((artist: any, index) => (
-                    <tr>
-                        <td>
-                            <p>{artist.title}</p>
-                        </td>
-                        <td>
-                            <p>{artist.description}</p>
-                        </td>
-                    </tr>
+                    <Table striped bordered hover size="sm">
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>isBand</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>
+                                <p>{artist.name}</p>
+                            </td>
+                            <td>
+                                <p>{artist.isBand}</p>
+                            </td>
+                        </tr>
+                    </tbody>
+
+                    </Table>
 
                 ))
             }
